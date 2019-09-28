cb0b6b874dc58790fb69f4118cfe1a0e0456b4f9
tfujimo1 <tfujimo1@gmail.com>
2019-09-29 01:12:47 +0900

    フォント描画まで

--- main.js
+++ main.js
@@ -3,26 +3,43 @@ Vue.use(Vuex);
 class CommandData {
     number;
     name;
-    constructor (number, name) {
+    dataList = [ ];
+    constructor (number, name, dataList) {
         this.number = number;
         this.name = name;
+        this.dataList = dataList;
+    }
+    drawPath(ctx) {
+        console.debug("drawPath : [" + this.name + "]");
+        if(this.name == "m") {
+            console.debug("m1");
+            ctx.moveTo(this.dataList[0], this.dataList[1]);
+        }
+        else if (this.name == "l") {
+            console.debug("l1");
+            ctx.lineTo(this.dataList[0], this.dataList[1]);
+        }
+        else if (this.name == "q") {
+            console.debug("q1");
+            ctx.quadraticCurveTo(this.dataList[2], this.dataList[3], this.dataList[0], this.dataList[1]);
+        }
+    }
+    get text () {
+        return this.name + " : " + this.dataList.join(" ");
     }
 }
 
 const store = new Vuex.Store({
     state: {
-        count: 0,
-        commandStr: "",
+        commandStr: "m 583 526 l 583 531 q 791 758 791 566 q 623 977 791 916 q 416 1006 541 1006 l 132 1006 l 132 14 l 454 14 q 660 48 580 14 q 828 290 828 121 q 583 526 828 483 m 252 576 l 252 907 l 408 907 q 665 745 665 907 q 400 576 665 576 l 252 576 m 252 113 l 252 482 l 407 482 q 570 456 494 482 q 699 297 699 412 q 437 113 699 113 l 252 113",
         commandList: [ ]
     },
     mutations: {
         clearCommand(state) {
-            state.count = 0;
             state.commandList = [];
         },
         addCommand(state, payload) {
-            state.count++;
-            state.commandList.push(new CommandData(state.count, payload.name));
+            state.commandList.push(payload.command);
         }
     }
 });
@@ -31,9 +48,6 @@ new Vue({
     el: '#app',
     store,
     computed: {
-        count () {
-            return this.$store.state.count;
-        },
         commandStr () {
             return this.$store.state.commandStr;
         },
@@ -49,28 +63,49 @@ function onclick_update(event) {
     const commandStr = $("#command-str").val();
     console.log("commandStr = " + commandStr);
     const tokenlist = commandStr.split(" ");
-    let currentToken = "";
-    let commandList = [];
-    tokenlist.forEach(token => {
-        console.log("token = " + token);
-        if (token === "q") {
-            if (currentToken != "") {
-                commandList.push(currentToken);
-            }
-            currentToken = token;
-        } else {
-            currentToken += (" " + token);
-        }
-    });
-    if (currentToken != "") {
-        commandList.push(currentToken);
-    }
+    let commandList = getCommandList(tokenlist);
     commandList.forEach(command => {
-        store.commit('addCommand', {name: command});
+        store.commit('addCommand', { command: command });
     });
-    updateCanvas();
+    updateCanvas(commandList);
 }
 
-function updateCanvas() {
-    let canvas = $("#command-str")[0];
+function getCommandList(tokenList) {
+    let i = 0;
+    let index = 1;
+    const commandList = new Array();
+    while (i < tokenList.length) {
+        if (tokenList[i] == "q") {
+            dataCount = 4;
+        }
+        else if (tokenList[i] == "m") {
+            dataCount = 2;
+        }
+        else if (tokenList[i] == "l") {
+            dataCount = 2;
+        }
+        const command = new CommandData(index++, tokenList[i], tokenList.slice(i+1, i+dataCount+1));
+        commandList.push(command);
+        i += (dataCount + 1);
+    }
+    return commandList;
+}
+
+function updateCanvas(commandList) {
+    let canvas = $("#canvas1")[0];
+    const ctx = canvas.getContext("2d");
+    ctx.save();
+    ctx.clearRect(0, 0, 300, 300);
+    ctx.lineWidth = 10;
+    ctx.fillStyle = "#AAA";
+    ctx.translate(0, 200);
+    ctx.scale(0.1, -0.1);
+    ctx.beginPath();
+    commandList.forEach(command => {
+        console.debug(command.name);
+        command.drawPath(ctx);
+    });
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
 }
